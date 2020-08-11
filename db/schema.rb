f5449bc3fb484d0735053996cf275c8f448d4203
Paula Pisa <pisa.paula@gmail.com>
2020-08-11 18:01:32 +0200

    Migrate error

--- db/schema.rb
+++ db/schema.rb
@@ -48,4 +48,7 @@ ActiveRecord::Schema.define(version: 2020_08_11_085042) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "rents", "services"
+  add_foreign_key "rents", "users"
 end
