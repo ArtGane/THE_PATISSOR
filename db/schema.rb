d0987935fd8f0a4846f9a15c667d26abe5814b60
Argane Samson <Argane.samson@gmail.com>
2020-08-12 15:05:44 +0200

    aaaaaah

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_11_085042) do
+ActiveRecord::Schema.define(version: 2020_08_12_124032) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,10 +45,9 @@ ActiveRecord::Schema.define(version: 2020_08_11_085042) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "address"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "rents", "services"
-  add_foreign_key "rents", "users"
 end
