03ff059ce5196d6049e04c48528883f7c73d0cac
Argane Samson <Argane.samson@gmail.com>
2020-08-13 12:31:53 +0200

    new dashboard

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :services, through: :rents
+  has_many :services
   has_many :rents
 end
