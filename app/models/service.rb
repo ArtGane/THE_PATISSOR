41e6d38391881709949ddf86b3e4ad61f5d56c2f
Simon Michel <michelsimontsin2@gmail.com>
2020-08-14 13:02:08 +0200

    dashboard bof

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,5 +1,5 @@
 class Service < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, dependent: :destroy
   validates :description, presence: true
   validates :price_per_hour, presence: true
 end
