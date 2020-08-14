86b6a004b10202f9273240fb64757ecb6c3295ea
Argane Samson <Argane.samson@gmail.com>
2020-08-14 15:27:39 +0200

    has_many rents on services

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,5 +1,6 @@
 class Service < ApplicationRecord
   belongs_to :user
+  has_many :rents, dependent: :destroy
   validates :description, presence: true
   validates :price_per_hour, presence: true
 end
