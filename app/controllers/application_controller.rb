7565b6e476c52f2f061b70a3d74a7f1ae3f2f527
Argane Samson <Argane.samson@gmail.com>
2020-08-17 10:01:31 +0200

    sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :authenticate_user!, only: :dashboard
+  before_action :authenticate_user!, except: [:home, :index]
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:address])
